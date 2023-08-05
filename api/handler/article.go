685827825feee102344e0217d73f0fa57d3c4c74
Sunchii <rachanlamoolphal@gmail.com>
2023-08-05 23:13:59 +0700

    create handler update article

--- api/handler/article.go
+++ api/handler/article.go
@@ -1,8 +1,11 @@
 package handler
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -80,3 +83,61 @@ func (articledb ArticleHandler) GetArticle(c *gin.Context) {
 	// Return the user as JSON response
 	c.JSON(http.StatusOK, article)
 }
+
+
+func (articledb ArticleHandler) UpdateArticle(c *gin.Context) {
+	// get id from param
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		errMsg := utils.BadRequestError("id should be number only")
+    log.Println(err)
+		c.JSON(errMsg.Status, errMsg.Message)
+    return
+	}
+
+	// Parse the request body to get the user data
+	var article models.Article
+	if err := c.ShouldBindJSON(&article); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// get user data
+	oldArticle, err := articledb.Db.GetArticle(strconv.Itoa(id))
+	if err != nil {
+		errMsg := utils.BadRequestError("does't exit")
+		c.JSON(errMsg.Status, errMsg.Message)
+		return
+	}
+
+	// prepare data befor insert to database
+	newArticle := models.Article{
+		ID:        int64(id),
+    Title: article.Title,
+    Description: article.Description,
+    Data: article.Data,
+    CreatedAt: time.Now(),
+	}
+
+	if article.Title == "" {
+		newArticle.Title = oldArticle.Title
+	}
+	if article.Description == "" {
+		newArticle.Description = oldArticle.Description
+	}
+	if len(article.Data) <=0 {
+		newArticle.Data = oldArticle.Data
+	}
+
+	// insert data to database
+	err = articledb.Db.UpdateArticle(&newArticle)
+	if err != nil {
+		errMsg := utils.InternalServerError("can't update data to database")
+		log.Println(err)
+		c.JSON(errMsg.Status, errMsg)
+		return
+	}
+	// Return a success message
+	c.JSON(http.StatusCreated, gin.H{"message": "User created successfully"})
+}
+
