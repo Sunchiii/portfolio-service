09983bb22bdd9bb62a618b1771aedb09a96b2426
Sunchii <rachanlamoolphal@gmail.com>
2023-08-05 22:37:34 +0700

    test format LSP

--- api/handler/article.go
+++ api/handler/article.go
@@ -1,9 +1,9 @@
 package handler
 
 import (
+	"log"
 	"net/http"
 	"time"
-  "log"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
@@ -12,72 +12,71 @@ import (
 	"github.com/sunchiii/portfolio-service/pkg/utils"
 )
 
-type ArticleHandler struct{
-  Db *database.DB
+type ArticleHandler struct {
+	Db *database.DB
 }
 
-func NewArticleHandler(db *database.DB)(ArticleHandler,error){
-  return ArticleHandler{Db: db},nil
+func NewArticleHandler(db *database.DB) (ArticleHandler, error) {
+	return ArticleHandler{Db: db}, nil
 }
 
-func (articledb ArticleHandler) CreateAtricle(c *gin.Context){
-  // prepare article object
-  var article models.Article 
+func (articledb ArticleHandler) CreateAtricle(c *gin.Context) {
+	// prepare article object
+	var article models.Article
 
-  // bind json data from user to article object
+	// bind json data from user to article object
 	if err := c.ShouldBindJSON(&article); err != nil {
-    errMsg := utils.BadRequestError("check your data request")
-    c.JSON(errMsg.Status,errMsg.Message)
+		errMsg := utils.BadRequestError("check your data request")
+		c.JSON(errMsg.Status, errMsg.Message)
 		return
 	}
 
-  // prepare newdata of article
-  newArticle := models.Article{
-    ID: int64(uuid.New().ID()),
-    Title: article.Title,
-    Description: article.Description,
-    Data: article.Data,
-    CreatedAt: time.Now(),
-  }
+	// prepare newdata of article
+	newArticle := models.Article{
+		ID:          int64(uuid.New().ID()),
+		Title:       article.Title,
+		Description: article.Description,
+		Data:        article.Data,
+		CreatedAt:   time.Now(),
+	}
 
-  // call function database ti create new article
-  err := articledb.Db.CreateArticle(&newArticle)
-  if err != nil{
-    errMsg := utils.InternalServerError("something wrong in server side")
-    c.JSON(errMsg.Status,errMsg.Message)
-    return
-  }
+	// call function database ti create new article
+	err := articledb.Db.CreateArticle(&newArticle)
+	if err != nil {
+		errMsg := utils.InternalServerError("something wrong in server side")
+		c.JSON(errMsg.Status, errMsg.Message)
+		return
+	}
 
-  c.JSON(http.StatusCreated, gin.H{"message": "Article created successfully"})
+	c.JSON(http.StatusCreated, gin.H{"message": "Article created successfully"})
 }
 
-func (articledb ArticleHandler) GetArticles(c *gin.Context){
-  // call database 
-  article, err := articledb.Db.GetArticles(1,10) 
-  if err != nil{
-    errMsg := utils.InternalServerError("can't get data from database")
-    log.Println(err)
-    c.JSON(errMsg.Status,errMsg.Message)
-    return
-  }
+func (articledb ArticleHandler) GetArticles(c *gin.Context) {
+	// call database
+	article, err := articledb.Db.GetArticles(1, 10)
+	if err != nil {
+		errMsg := utils.InternalServerError("can't get data from database")
+		log.Println(err)
+		c.JSON(errMsg.Status, errMsg.Message)
+		return
+	}
 
-  c.JSON(http.StatusOK,article)
+	c.JSON(http.StatusOK, article)
 }
 
-func (articledb ArticleHandler) GetArticle(c *gin.Context){
+func (articledb ArticleHandler) GetArticle(c *gin.Context) {
 	// Get the user ID from the request parameters
 	articleID := c.Param("id")
 	// Retrieve the user from the database or any other data source
-  
-  article,err := articledb.Db.GetArticle(articleID)
-  if err != nil{
-    errMsg := utils.InternalServerError("can't query data from database")
-    log.Println(err)
-    c.JSON(errMsg.Status,errMsg.Message)
-    return
-  }
+
+	article, err := articledb.Db.GetArticle(articleID)
+	if err != nil {
+		errMsg := utils.InternalServerError("can't query data from database")
+		log.Println(err)
+		c.JSON(errMsg.Status, errMsg.Message)
+		return
+	}
 
 	// Return the user as JSON response
 	c.JSON(http.StatusOK, article)
 }
-
--- api/handler/user.go
+++ api/handler/user.go
@@ -14,12 +14,12 @@ import (
 	"github.com/sunchiii/portfolio-service/pkg/utils"
 )
 
-type UserHandler struct{
-  Db *database.DB
+type UserHandler struct {
+	Db *database.DB
 }
 
-func NewUserHandler(db *database.DB)(UserHandler,error){
-  return UserHandler{Db: db},nil
+func NewUserHandler(db *database.DB) (UserHandler, error) {
+	return UserHandler{Db: db}, nil
 }
 
 func (users *UserHandler) CreateUserHandler(c *gin.Context) {
@@ -29,36 +29,35 @@ func (users *UserHandler) CreateUserHandler(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-  
-  // prepare data befor insert to database
-  newUser := models.User{
-    ID: int64(uuid.New().ID()),
-    Username: user.Username,
-    Password: user.Password,
-    CreatedAt: time.Now(),
-  } 
 
+	// prepare data befor insert to database
+	newUser := models.User{
+		ID:        int64(uuid.New().ID()),
+		Username:  user.Username,
+		Password:  user.Password,
+		CreatedAt: time.Now(),
+	}
 
-  // insert data to database
-  err := users.Db.CreateUser(&newUser)
-  if err != nil {
-    errMsg := utils.InternalServerError("can't insert data to database or username already exit")
-    log.Println(err)
-    c.JSON(errMsg.Status,errMsg)
-    return
-  }
+	// insert data to database
+	err := users.Db.CreateUser(&newUser)
+	if err != nil {
+		errMsg := utils.InternalServerError("can't insert data to database or username already exit")
+		log.Println(err)
+		c.JSON(errMsg.Status, errMsg)
+		return
+	}
 	// Return a success message
 	c.JSON(http.StatusCreated, gin.H{"message": "User created successfully"})
 }
 
 func (users *UserHandler) UpdateUserHandler(c *gin.Context) {
-  // get id from param
-  id,err := strconv.Atoi(c.Param("id"))
-  if err != nil{
-    errMsg := utils.BadRequestError("id should be number only")
-    c.JSON(errMsg.Status,errMsg.Message)
-  }
-  
+	// get id from param
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		errMsg := utils.BadRequestError("id should be number only")
+		c.JSON(errMsg.Status, errMsg.Message)
+	}
+
 	// Parse the request body to get the user data
 	var user models.User
 	if err := c.ShouldBindJSON(&user); err != nil {
@@ -66,68 +65,63 @@ func (users *UserHandler) UpdateUserHandler(c *gin.Context) {
 		return
 	}
 
-  // get user data
-  oldUser, err := users.Db.GetUser(strconv.Itoa(id))
-  if err != nil{
-    errMsg := utils.BadRequestError("does't exit")
-    c.JSON(errMsg.Status,errMsg.Message)
-    return
-  }
-  
-  // prepare data befor insert to database
-  newUser := models.User{
-    ID: int64(id),
-    Username: user.Username,
-    Password: user.Password,
-    CreatedAt: oldUser.CreatedAt,
-  } 
+	// get user data
+	oldUser, err := users.Db.GetUser(strconv.Itoa(id))
+	if err != nil {
+		errMsg := utils.BadRequestError("does't exit")
+		c.JSON(errMsg.Status, errMsg.Message)
+		return
+	}
 
-  if user.Username == ""{
-    newUser.Username = oldUser.Username
-  }
-  if user.Password == ""{
-    newUser.Password = oldUser.Password
-  }
+	// prepare data befor insert to database
+	newUser := models.User{
+		ID:        int64(id),
+		Username:  user.Username,
+		Password:  user.Password,
+		CreatedAt: oldUser.CreatedAt,
+	}
 
+	if user.Username == "" {
+		newUser.Username = oldUser.Username
+	}
+	if user.Password == "" {
+		newUser.Password = oldUser.Password
+	}
 
-
-  // insert data to database
-  err = users.Db.UpdateUser(&newUser)
-  if err != nil {
-    errMsg := utils.InternalServerError("can't update data to database")
-    log.Println(err)
-    c.JSON(errMsg.Status,errMsg)
-    return
-  }
+	// insert data to database
+	err = users.Db.UpdateUser(&newUser)
+	if err != nil {
+		errMsg := utils.InternalServerError("can't update data to database")
+		log.Println(err)
+		c.JSON(errMsg.Status, errMsg)
+		return
+	}
 	// Return a success message
 	c.JSON(http.StatusCreated, gin.H{"message": "User created successfully"})
 }
-
 
 func (users *UserHandler) GetUserHandler(c *gin.Context) {
 	// Get the user ID from the request parameters
 	userID := c.Param("id")
 	// Retrieve the user from the database or any other data source
-  
-  user,err := users.Db.GetUser(userID)
-  if err != nil{
-    errMsg := utils.InternalServerError("can't query data from database")
-    log.Println(errMsg.Message)
-    c.JSON(errMsg.Status,errMsg.Message)
-  }
+
+	user, err := users.Db.GetUser(userID)
+	if err != nil {
+		errMsg := utils.InternalServerError("can't query data from database")
+		log.Println(errMsg.Message)
+		c.JSON(errMsg.Status, errMsg.Message)
+	}
 
 	// Return the user as JSON response
 	c.JSON(http.StatusOK, user)
 }
 
-
-
 func (users *UserHandler) GetUsersHandler(c *gin.Context) {
-  user,err := users.Db.GetUsers()
-  if err != nil{
-    errMsg := utils.InternalServerError("can't query data in database")
-    c.JSON(errMsg.Status,errMsg.Message)
-  }
+	user, err := users.Db.GetUsers()
+	if err != nil {
+		errMsg := utils.InternalServerError("can't query data in database")
+		c.JSON(errMsg.Status, errMsg.Message)
+	}
 	// Retrieve the user from the database or any other data source
 	// For demonstration purposes, let's assume we have a user with ID 1
 
@@ -135,19 +129,18 @@ func (users *UserHandler) GetUsersHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, user)
 }
 
-
 func (users *UserHandler) DeleteUserHandler(c *gin.Context) {
 	// Get the user ID from the request parameters
 	userID := c.Param("id")
 
-  // call database
-  err := users.Db.DeleteUser(userID)
-  if err != nil{
-    errMsg := utils.InternalServerError("something wrong in server")
-    log.Println(err)
-    c.JSON(errMsg.Status,errMsg.Message)
-    return
-  }
+	// call database
+	err := users.Db.DeleteUser(userID)
+	if err != nil {
+		errMsg := utils.InternalServerError("something wrong in server")
+		log.Println(err)
+		c.JSON(errMsg.Status, errMsg.Message)
+		return
+	}
 	// Return a success message
 	c.JSON(http.StatusOK, gin.H{"message": "User deleted successfully"})
 }
@@ -156,15 +149,14 @@ func (articledb *ArticleHandler) GetArticleHandler(c *gin.Context) {
 	// Get the user ID from the request parameters
 	id := c.Param("id")
 	// Retrieve the user from the database or any other data source
-  
-  article, err := articledb.Db.GetArticle(id)
-  if err != nil{
-    errMsg := utils.InternalServerError("can't query data from database")
-    log.Println(errMsg.Message)
-    c.JSON(errMsg.Status,errMsg.Message)
-  }
+
+	article, err := articledb.Db.GetArticle(id)
+	if err != nil {
+		errMsg := utils.InternalServerError("can't query data from database")
+		log.Println(errMsg.Message)
+		c.JSON(errMsg.Status, errMsg.Message)
+	}
 
 	// Return the user as JSON response
 	c.JSON(http.StatusOK, article)
 }
-
